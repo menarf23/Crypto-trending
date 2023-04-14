a31ebf322afda436ee2ab39b3b10fba46aa6fddf
menarf23 <franemilovcic@gmail.com>
2023-04-14 19:47:47 +0200

    Made a separate component for trending coins table

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,56 @@
 import './App.css';
 // import mockData from "./mock_data.json";
-// import axios from 'axios';
+import axios from 'axios';
 import TrendingCoinsTable from './TrendingCoinsTable';
+// import RawData from './RawData';
 import Header from './Header';
 import Footer from './Footer';
 import * as React from "react";
-// import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState } from 'react';
 // import {useTable} from "react-table";
 
 function App() {
 
+  const [trendingCoins, setTrendingCoins] = useState([]);
+  const [favoriteCoins, setFavoriteCoins] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://api.coingecko.com/api/v3/search/trending`)
+    .then(response => {
+      console.log("Raw data: ", response);
+      const originalCoins = response.data.coins;
+      originalCoins.forEach(element => {
+        element.item.favorite_status = false;
+      });
+      console.log("Raw coins: ", response.data.coins);
+      setTrendingCoins(originalCoins);
+    });
+  }, []); 
+
+  console.log("Trending Coins: ", trendingCoins);
+  console.log("Favorite Coins: ", favoriteCoins);
+
+
+
+  function toggleFavoriteStatus(row) {
+    const coinToUpdateIndex = trendingCoins.findIndex(coin => coin.item.coin_id === row.original.item.coin_id
+      );
+    const temporaryTrendingCoins = [...trendingCoins];
+    temporaryTrendingCoins[coinToUpdateIndex].item.favorite_status = !row.original.item.favorite_status;
+    setTrendingCoins(temporaryTrendingCoins);
+
+    const temporaryFavoriteCoins = temporaryTrendingCoins.filter (element => element.item.favorite_status === true);
+    setFavoriteCoins(temporaryFavoriteCoins);
+    // const temporaryCoins = [...trendingCoins];
+    // temporaryCoins[id].item.favorite_status = !trendingCoins[id].item.favorite_status;
+    // setTrendingCoins(temporaryCoins);
+  }
+
   return (
     <div className="App">
       <Header />
-      <TrendingCoinsTable />
+      
+      <TrendingCoinsTable trendCoins={trendingCoins} favoriteStatus={toggleFavoriteStatus}/>
 
       
 
--- src/components/TrendingCoinsTable copy.js
+++ src/components/TrendingCoinsTable copy.js
@@ -2,17 +2,55 @@ import './App.css';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import Fab from '@mui/material/Fab';
+import axios from 'axios';
 import * as React from "react";
-import { useMemo } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import {useTable} from "react-table";
 
 
-function TrendingCoinsTable(props) {
+function TrendingCoinsTable() {
 
-  const trendCoins = props.trendCoins;
+  const [trendingCoins, setTrendingCoins] = useState([]);
+  const [favoriteCoins, setFavoriteCoins] = useState([]);
+
+  
+  useEffect(() => {
+    axios.get(`https://api.coingecko.com/api/v3/search/trending`)
+    .then(response => {
+      console.log("Raw data: ", response);
+      const originalCoins = response.data.coins;
+      originalCoins.forEach(element => {
+        element.item.favorite_status = false;
+      });
+      console.log("Raw coins: ", response.data.coins);
+      setTrendingCoins(originalCoins);
+    });
+  }, []); 
+  
+
+  // console.log("List of trending coins: ", trendingCoins);
+
+  
+
+  function toggleFavoriteStatus(row) {
+    console.log(row);
+    const coinToUpdateIndex = trendingCoins.findIndex(coin => coin.item.coin_id === row.original.item.coin_id
+      );
+    const temporaryTrendingCoins = [...trendingCoins];
+    temporaryTrendingCoins[coinToUpdateIndex].item.favorite_status = !row.original.item.favorite_status;
+    setTrendingCoins(temporaryTrendingCoins);
+
+    const temporaryFavoriteCoins = temporaryTrendingCoins.filter (element => element.item.favorite_status === true);
+    setFavoriteCoins(temporaryFavoriteCoins);
+    // const temporaryCoins = [...trendingCoins];
+    // temporaryCoins[id].item.favorite_status = !trendingCoins[id].item.favorite_status;
+    // setTrendingCoins(temporaryCoins);
+  }
+
+  console.log("Favorite coins: ", favoriteCoins);
   
   
-  const tableData = useMemo(() => trendCoins, [trendCoins]);
+  const tableData = useMemo(() => trendingCoins, [trendingCoins]);
   
   const columns = useMemo(() => [
     {
@@ -45,12 +83,12 @@ function TrendingCoinsTable(props) {
     {
       Header: "FAVORITE",
       Cell: tableProps => (
-        <Fab size="medium" onClick={() => props.favoriteStatus(tableProps.row)}>
+        <Fab size="medium" onClick={() => toggleFavoriteStatus(tableProps.row)}>
         {tableProps.row.original.item.favorite_status ? <FavoriteIcon/> : <FavoriteBorderIcon/> }
         </Fab>),
       accessor: "item.favorite_status",
     }
-  ], [trendCoins]);
+  ], [trendingCoins]);
 
   
 
@@ -59,7 +97,7 @@ function TrendingCoinsTable(props) {
   const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} 
   = useTable({columns, data: tableData});
 
-  // console.log("Rows: ", rows);
+  console.log("Rows: ", rows);
 
   return (
     <div>
